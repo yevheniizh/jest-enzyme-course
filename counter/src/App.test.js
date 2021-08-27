3b2adbe7a6da4c8419a22993ed89dca7215ef12d
Yevhenii Zhyrov <zh.zhyrov@gmail.com>
2021-08-27 11:18:08 +0300

    add counter folder

---
+++ counter/src/App.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+import App from './App';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const setup = () => shallow(<App />);
+
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test="${val}"]`);
+
+test('renders without crashing', () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
+  expect(appComponent.length).toBe(1);
+});
+
+test('renders increment button', () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, 'increment-button');
+  expect(button.length).toBe(1);
+});
+
+test('renders counter display', () => {
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.length).toBe(1);
+});
+
+test('counter display starts at 0', () => {
+  const wrapper = setup();
+  const count = findByTestAttr(wrapper, 'count').text();
+  expect(count).toBe('0');
+});
+
+test('clicking button increments counter display', () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, 'increment-button');
+  button.simulate('click');
+
+  const count = findByTestAttr(wrapper, 'count').text();
+  expect(count).toBe('1');
+});
